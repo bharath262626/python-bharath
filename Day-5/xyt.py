0cc6187569f4e76d8c5dc7241dfc5209f6b3991a
bharath262626 <bharathyadav262626@gmail.com>
2025-08-22 23:57:06 -0700

    xyt file

---
+++ Day-5/xyt.py
@@ -0,0 +1,13 @@
+my_file = ["ec2", "ec2-1", "ec22", "ec2-3", "ec2-4"]
+
+for vms in my_file:
+    if vms == "ec22":
+        break
+    print(vms)
+        
+
+for vm2 in my_file:
+    if vm2 == "ec22":
+        continue
+    print(vm2)
+
